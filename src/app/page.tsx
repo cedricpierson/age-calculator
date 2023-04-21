ea17838a74d9c6f813cc98c6ae6fbf7b99f3b443
Cédric PIERSON <cdric.pierson@gmail.com>
2023-04-21 14:42:33 +0200

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,124 +1,117 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
+import { NextPage } from "next";
+import React, { useState } from "react";
 
-export default function Home() {
+const page: NextPage = () => {
+  const [days, setDays] = useState("DD");
+  const [months, setMonths] = useState("MM");
+  const [years, setYears] = useState("YYYY");
+  const [ageDays, setAgeDays] = useState("- -");
+  const [ageMonths, setAgeMonths] = useState("- -");
+  const [ageYears, setAgeYears] = useState("- -");
+  //------------------------------------------------------
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (days < new Date().getDate()) {
+      setAgeDays(new Date().getDate() - days);
+    } else if (days > new Date().getDate()) {
+      setAgeDays(
+        new Date(years, months, 0).getDate() + new Date().getDate() - days
+      );
+    }
+
+    if (months < new Date().getMonth()) {
+      setAgeMonths(new Date().getMonth() + 1 - months);
+      setAgeYears(new Date().getFullYear() - years);
+    } else {
+      setAgeMonths(12 + new Date().getMonth() + 1 - months);
+      setAgeYears(new Date().getFullYear() - years - 1);
+    }
+    if (months == new Date().getMonth() + 1 && days == new Date().getDate()) {
+      setAgeYears(new Date().getFullYear() - years);
+      setAgeMonths(0);
+      setAgeDays(0);
+    }
+  };
+  //___________________________________________________________________________________________________
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="flex justify-center items-center w-full h-screen   bg-[#DBDBDB]">
+      <div className="p-14 w-7/12 h-fit rounded-3xl rounded-br-[200px] bg-white">
+        <form className="flex flex-row" onSubmit={handleSubmit}>
+          <div className="flex flex-col">
+            <label
+              htmlFor="day"
+              className="text-slate-500 font-semibold tracking-widest mb-2"
+            >
+              DAY
+            </label>
+            <input
+              required
+              min="1"
+              max="31"
+              type="number"
+              onChange={(e) => setDays(e.target.value)}
+              placeholder={"DD"}
+              className="cursor-pointer mr-8 px-6 py-2.5 border border-slate-300 rounded-md w-40 focus:border-purple-500 focus:outline-none text-3xl font-bold"
             />
-          </a>
+          </div>
+          <div className="flex flex-col">
+            <label
+              htmlFor="month"
+              className="text-slate-500 font-semibold tracking-widest mb-2"
+            >
+              MONTH
+            </label>
+            <input
+              required
+              min="1"
+              max="12"
+              type="number"
+              onChange={(e) => setMonths(e.target.value)}
+              placeholder="MM"
+              className="cursor-pointer mr-8 px-6 py-2.5 border border-slate-300 rounded-md w-40 focus:border-purple-500 focus:outline-none text-3xl font-bold"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label
+              htmlFor="year"
+              className="text-slate-500 font-semibold tracking-widest mb-2"
+            >
+              YEAR
+            </label>
+            <input
+              required
+              min="1"
+              max={new Date().getFullYear()}
+              type="number"
+              onChange={(e) => setYears(e.target.value)}
+              placeholder="YYYY"
+              className="cursor-pointer mr-8 px-6 py-2.5 border border-slate-300 rounded-md w-40 focus:border-purple-500 focus:outline-none text-3xl font-bold"
+            />
+          </div>
+          <button className="hover:scale-105 ease-in duration-300 left-16 top-24 cursor-pointer circle bg-purple-600 hover:bg-black">
+            <div className="arrow"></div>
+            <div className="left"></div>
+            <div className="right"></div>
+          </button>
+        </form>
+        <hr className="my-12 border mr-20 border-slate-200" />
+        <div className="">
+          <h1 className=" text-8xl font-extrabold italic">
+            <span className="text-purple-600">{ageYears}</span> years
+          </h1>
+          <h1 className=" text-8xl font-extrabold italic">
+            <span className="text-purple-600">{ageMonths}</span> months
+          </h1>
+          <h1 className=" text-8xl font-extrabold italic">
+            <span className="text-purple-600">{ageDays}</span> days
+          </h1>
         </div>
       </div>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default page;
